d541d1f99fb4488abc95c59303645511fc91f646
René van den Berg <r.van.den.berg@ogd.nl>
2012-04-23 20:27:17 +0200

    Boyscout: use transactional fixtures in spec runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,4 +19,6 @@ $LOAD_PATH << File.expand_path('../../app/controllers', __FILE__)
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
+
+  config.use_transactional_fixtures = true
 end
