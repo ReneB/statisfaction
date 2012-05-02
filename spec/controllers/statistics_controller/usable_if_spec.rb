5ee90d28c24a3d82ee20c641601e61451dd4ed09
Lennaert Meijvogel <l.meijvogel@ogd.nl>
2012-05-02 12:31:12 +0200

    Boyscout: Clean up after StatisticsController#usable_if spec.

--- spec/controllers/statistics_controller/usable_if_spec.rb
+++ spec/controllers/statistics_controller/usable_if_spec.rb
@@ -2,6 +2,14 @@ require "spec_helper"
 
 describe Statisfaction::StatisticsController do
   describe :usable_if do
+    before(:each) do
+      @old_value = Statisfaction::StatisticsController.class_variable_get(:@@access_specification)
+    end
+
+    after(:each) do
+      Statisfaction::StatisticsController.class_variable_set(:@@access_specification, @old_value)
+    end
+
     it "should set @@access_specification" do
       block = proc { true }
 
